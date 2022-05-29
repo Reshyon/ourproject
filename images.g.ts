a12b864a88174565303437e9d7fb6c5a17a1b55a
Tiia <95775616+Reshyon@users.noreply.github.com>
2022-05-29 11:14:49 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,72 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "hyttynen1":return img`
+. . d d b b c . . . . . . . . . 
+. d 1 1 d c c c . . . . . . . . 
+. 1 1 1 1 1 c c . . . . . . . . 
+. . 1 1 1 1 d c . . . . . . . . 
+. . . 1 1 1 1 d e . . f f f . . 
+. . . . c d 1 1 e c f f b f f f 
+. . . . . c f f 5 c f f f f . . 
+. . . . e e 5 b c 4 f . . . . . 
+. . b e b b c b c . b b b . . . 
+. b e b d b 5 . . c . . . b . . 
+. b 5 b f b c . . . c c . . d . 
+b . b b c . c . . . . . c . . . 
+. . c c d . . c . . . . . b . . 
+. c . . . . . d . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . d d b b c . . . . . . . . . 
+. d 1 1 d c c c . . . . . . . . 
+. 1 1 1 1 1 c c . . . . . . . . 
+. . 1 1 1 1 d c e . . f f f . . 
+. . . 1 1 1 1 d e c f f b f f f 
+. . . . c d 1 1 5 c f f f f . . 
+. . . . e e 5 b c 4 f . . . . . 
+. . b e b b c b c . b b b . . . 
+. b e b d b 5 . . c . . . b . . 
+. b 5 b f b c . . . c c . . d . 
+b . b b c . c . . . . . c . . . 
+. . c c d . . c . . . . . b . . 
+. c . . . . . d . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . d d b b c . . . . . . . . . 
+. d 1 1 d c c c . . . . . . . . 
+. 1 1 1 1 1 c c . . . . . . . . 
+. . 1 1 1 1 d c . . . . . . . . 
+. . . 1 1 1 1 d e . . f f f . . 
+. . . . c d 1 1 e c f f b f f f 
+. . . . . c f f 5 c f f f f . . 
+. . . . e e 5 b c 4 f . . . . . 
+. . b e b b c b c . b b b . . . 
+. b e b d b 5 . . c . . . b . . 
+. b 5 b f b c . . . c c . . d . 
+b . b b c . c . . . . . c . . . 
+. . c c d . . c . . . . . b . . 
+. c . . . . . d . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
